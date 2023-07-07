7d00f76ad80ada5ba612c905f678030a6ea04924
Lisa Lin <llin1@rusty1.cm.cluster>
2023-07-07 12:46:27 -0400

    minor tweaks

--- jobscript.sh
+++ jobscript.sh
@@ -18,20 +18,9 @@ source /etc/profile #commands in the file will be executed as they were executed
 
 # ORDER OF STEPS:
 # 1. Generate simulation parameters with generateparams.py
-# e.g. python generateparams.py --xvar Dg_ratio --xb 0 1 25 --yvar theta_c0_ratio --yb 0.01 0.99 25 --outdir test
 
 # 2. Simulate system using these parameters - make sure that [total number of jobs being submitted (--array)] x [number of tasks per job (per_task)] = total number of different parameter combos. You can check the file length of the parameter file with "wc -l [parameter file path]". Subtract 1 from the final count to account for the header row.
-# NOTE: per_task = 25 takes around 15 min to run
-# e.g. sbatch myscript.sh
-
-# 3. Compile all generated simulation files into one .csv file using CompileData.py. Make sure you have loaded anaconda before running this step (module load anaconda)!
-# e.g. python CompileData.py [output directory path]
-
-# 4. Copy resulting output file back to your local machine to perform any plotting routines desired, i.e. plotting a phase diagram
-# e.g. scp [source] [directory]
-# NOTE: actually, I just realized I can plot things using python in Supercloud! (I think I was having issues previously plotting anything in MATLAB)
-# in which case, plot using PlotPhaseDiagrams.py, 
-# e.g. python PlotPhaseDiagrams.py K0_mu_res4_tmax50_2500/K0_mu_res4_tmax50_2500_combinedata.csv K0 mu avgval
+# e.g. sbatch jobscript.sh
 
 #run simulation function
 start=`date +%s.%N`
